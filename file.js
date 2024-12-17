326db15a7095d5991134c6a9a2d767f63be7fd72
Haseebqureshi947 <abdulhaseebqureshi518@gmail.com>
2024-12-18 02:05:26 +0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,59 @@
+//chapter 1//
+//question 1
+//alert("Welcome To JS");
+
+//question 2
+//alert("Error! please enter a valid pasword");
+
+//question 3
+//alert("Hello JS\nHappy Coding");
+
+//question 4//
+//alert("Welcome to JS land ")
+//alert("Happy Coding \n prevent this page from creating additional dialogs");
+
+//question 5
+//console.log("hello !I can run JS through my web browser's console");
+
+//question 6
+//alert("hi i'm abdul haseeb");
+
+//chapter 2//
+//question 1
+
+//const fullName=("abdul haseeb");
+//alert(fullName);
+
+//const message=("Hello ");
+//alert(message);
+
+//const biodata=(" certified web designer");
+//alret(biodata);
+
+
+
+//question 2
+//const foodName=("pizza\npizz\npiz\npi\np");
+//alert(foodName);
+
+//question 3
+//let email=("abdulhaseebqureshi518@gmail.com");
+//alert(email)
+
+//question 4
+//var book=("Asmarter way to learn JavaScrip.");
+//alert(book)
+
+//chapter 3
+
+//question 1
+//let age=("I am 19 years old");
+//alert(age);
+
+//question 2
+
+//let visitTime=("You have visited this site 15 times");
+//alert(visitTime)
+
+
+
